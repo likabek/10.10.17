3680a7d5385aac93f8b25c8ff1231a3275c9b1bc
tech-lab <tech-lab@itc-lab>
2017-10-10 21:00:39 +0400

    if statements, loops

---
+++ Hasmik.js
@@ -0,0 +1,39 @@
+/* var z = 824;
+var lgt = "dfhjk";
+var h = false
+console.log(z);
+console.log(lgt);
+console.log(h);
+name = "abc"
+console.log(name);
+var k;
+console.log(k);
+let a = 6;
+if ( a % 2 === 0)
+console.log("a is even");
+else if ( a % 2 !=== 0)
+console.log("a is odd");
+else
+console.log("a = 0");
+let a=1;
+a++;
+console.log(a);
+
+++a;
+console.log(a);
+
+let a="ab  a";
+console.log(!!a)
+
+
+let a=30;
+if(a===0)
+console.log("0");
+else
+console.log("not 0");
+
+*/
+let a=0;
+
+while(a <= 100)
+console.log(++a);
